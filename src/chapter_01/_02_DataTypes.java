d4a6b3d66b565bd4e9491e87b68786ce348f370b
Rowoon Lee <dev.rowoon@gmail.com>
2023-06-15 11:04:11 +0900

    자료형 알아보기

---
+++ src/chapter_01/_02_DataTypes.java
@@ -0,0 +1,19 @@
+package chapter_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        // 문자
+        System.out.println("Hello World");
+        System.out.println("안녕하세요");
+        // 숫자
+        System.out.println(12);
+        System.out.println(-34);
+        System.out.println(3.14);
+        // 불리언
+        System.out.println(true);
+        System.out.println(false);
+
+        // 숫자 계산
+        System.out.println(123 + 345);
+    }
+}
