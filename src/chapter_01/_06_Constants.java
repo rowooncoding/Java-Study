016b08707cafc28d555daa402c85ed67b33cdf3c
Rowoon Lee <dev.rowoon@gmail.com>
2023-06-16 17:17:35 +0900

    :art: 상수 알아보기

---
+++ src/chapter_01/_06_Constants.java
@@ -0,0 +1,12 @@
+package chapter_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        // 🧐 상수 만드는 방법
+        final String KR_COUNTRY_CODE = "+82";
+
+        // 🧐 상수 활용 방법
+        final double PI = 3.141592; // 원주율은 바꿀 필요 없다.
+        final String DATE_OF_BIRTH = "2001-12-31"; // 생일도 안바뀐다.
+    }
+}
